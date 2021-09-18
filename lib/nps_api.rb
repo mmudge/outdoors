83611017aa02dcedae32482c174a78f4e8b02993
michael mudge <michaeltmudge@gmail.com>
2021-09-17 23:16:11 -0700

    paginate through nps_api

--- lib/nps_api.rb
+++ lib/nps_api.rb
@@ -4,7 +4,7 @@ class NpsApi
     api_key = Rails.application.credentials[:nps_api_key]
     @options = {
       headers: { "Content-Type": "application/json" },
-      query: { api_key: api_key },
+      query: { api_key: api_key},
       format: 'plain'
     }
   end
@@ -14,10 +14,22 @@ class NpsApi
   def get_parks
     url = BASE_URL + 'parks'
 
-    @response ||= HTTParty.get(url, @options)
+    parks = []
+    @options[:query][:start] = 0
+    @options[:query][:limit] = 50
 
-    # puts response.body
+    loop do
+      response = HTTParty.get(url, @options)
+      parks_data = JSON.parse(response.body)['data']
 
-    JSON.parse(@response.body)['data']
+      parks += parks_data
+
+      @options[:query][:start] = @options[:query][:limit]
+      @options[:query][:limit] = @options[:query][:limit] + 50
+
+      break if parks_data.length < 1
+    end
+
+    parks
   end
 end
